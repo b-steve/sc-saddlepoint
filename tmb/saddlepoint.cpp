3e2fc46718aa0fcc41382f7667e7019563097268
Ben Stevenson <ben.stevenson@auckland.ac.nz>
2023-07-06 12:13:06 +1200

    TMB functions for KGF/MGF conditional on activity centre.

--- tmb/saddlepoint.cpp
+++ tmb/saddlepoint.cpp
@@ -5,10 +5,22 @@
 template<class Type>
 Type objective_function<Type>::operator() ()
 {
-  // Reading in data.
-  DATA_IVECTOR(counts);
-  DATA_MATRIX(mask);
+  // Survey setup.
   DATA_MATRIX(traps);
+  // Activity centres.
+  DATA_VECTOR(s);
+  // Model parameters.
+  DATA_SCALAR(lambda0);
+  DATA_SCALAR(sigma);
+  // Indicator for nonzero counts.
+  DATA_IVECTOR(nonzero);
+  // Function to return.
+  DATA_STRING(fn);
+  // Argument to the KGF.
+  PARAMETER_VECTOR(t);
   Type obj = 0;
-  return -obj;
+  if (fn == "kgf_x_given_s"){
+    obj += kgf_x_given_s(t, s, traps, lambda0, sigma, nonzero);
+  }
+  return obj;
 }
--- tmb/utilities.h
+++ tmb/utilities.h
@@ -3,15 +3,6 @@
 
 // Stable Poisson PMF.
 template<class Type>
-Type dpois_stable (const Type &x, const Type &lambda, const int &give_log){
-  Type out;
-  out = pow(lambda, x)*exp(-lambda)/exp(lgamma(x + 1));
-  if (give_log){
-    out = log(out + DBL_MIN);
-  }
-  return out;
-}
-template<class Type>
 Type dpois_stable (const int &x, const Type &lambda, const int &give_log){
   double d_x = x;
   Type out;
@@ -24,38 +15,42 @@ Type dpois_stable (const int &x, const Type &lambda, const int &give_log){
 
 // KGF for the Poisson distribution.
 template<class Type>
-Type kgf_pois(const Type &t, const double &lambda){
-  lambda*(exp(t) - 1);
+Type kgf_pois(const Type &t, const Type &lambda){
+  return lambda*(exp(t) - 1);
 }
 
 // MGF for the Poisson distribution.
 template<class Type>
-Type mgf_pois(const Type &t, const double &lambda){
-  exp(kgf_pois(t, lambda));
+Type mgf_pois(const Type &t, const Type &lambda){
+  return exp(kgf_pois(t, lambda));
 }
 
 // KGF for a single individual's counts.
 template<class Type>
-Type kgf_x_given_s(const vector<Type> &t, const matrix<double> &s, const matrix<Type> &traps, const double &lambda0, const double &sigma, const vector<double> &nz){
-  int n_s = s.size();
-  int n_traps = traps.size();
-  double d;
-  double lambda;
+Type kgf_x_given_s(const vector<Type> &t, const vector<Type> &s, const matrix<Type> &traps, const Type &lambda0, const Type &sigma, const vector<int> nonzero){
+  int n_traps = traps.rows();
+  Type d;
+  Type lambda;
   Type out = 0;
-  for (int i = 0; i < n_s; i++){
-    for (int j = 0; j < n_traps; j++){
-      d = pow(pow(s(i, 1) - traps(j, 1), 2) +
-	      pow(s(i, 2) - traps(j, 2), 2), 0.5);
-      lambda = lambda0*exp(-pow(d, 2)/(2*pow(sigma, 2)));
-      if (nz(i) == 1){
-	out += kgf_pois(t, lambda);
-      } else {
-	out += dpois_stable(0, lambda, 1);
-      }
+  int k = 0;
+  for (int i = 0; i < n_traps; i++){
+    d = pow(pow(s(0) - traps(i, 0), 2) +
+	    pow(s(1) - traps(i, 1), 2), 0.5);
+    lambda = lambda0*exp(-pow(d, 2)/(2*pow(sigma, 2)));
+    if (nonzero(i) == 1){
+      out += kgf_pois(t(k), lambda);
+      k++;
+    } else {
+      out += dpois_stable(0, lambda, 1);
     }
   }
   return out;
 }
 
+// MGF for a single individual's counts.
+template<class Type>
+Type mgf_x_given_s(const vector<Type> &t, const vector<Type> &s, const matrix<Type> &traps, const Type &lambda0, const Type &sigma, const vector<int> nonzero){
+  return exp(mgf_x_given_s(t, s, traps, lambda0, sigma, nonzero));
+}
 
 #endif
